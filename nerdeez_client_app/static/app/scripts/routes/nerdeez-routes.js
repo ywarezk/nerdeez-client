6fd245f30b1c2df7ceeba714c1213036b6b99ea5
Yariv Katz <yariv@nerdeez.com>
2013-07-12 21:14:00 +0300

    define the routes for the search

--- nerdeez_client_app/static/app/scripts/routes/nerdeez-routes.js
+++ nerdeez_client_app/static/app/scripts/routes/nerdeez-routes.js
@@ -7,10 +7,19 @@
 * @copyright: nerdeez.com Ltd.
 */
 
+/**
+ * define the routes urls here
+ */
 Nerdeez.Router.map(function () {
-  // put your routes here
+	this.resource('search', function() {
+	    this.route('university');
+	    this.route('course');
+	});
 });
 
+/**
+ * the application route
+ */
 Nerdeez.IndexRoute = Ember.Route.extend({
 
 	/**
