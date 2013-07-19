ed789a3c476d63add43bc6bbfb79284dd248b7ab
OpenShift guest <ceo@ykapps.com>
2013-07-19 03:10:20 -0400

    make the project work on django

--- nerdeez_client_app/views.py
+++ nerdeez_client_app/views.py
@@ -1,6 +1,26 @@
 '''
 nerdeez client views will be defined here
+
+@copyright: nerdeez.com Ltd.
+@version: 1.0
+@author: Yariv Katz
 '''
+
+#=============================
+# begin imports
+#=============================
+
+import os
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+#=============================
+# end imports
+#=============================
+
+#=============================
+# begin application views
+#=============================
 
 def spa(request):
     '''
@@ -8,5 +28,12 @@ def spa(request):
     @param request: the request object
     '''
     
+    return render_to_response(
+        'base.html',
+        {},
+        context_instance=RequestContext(request)
+        )
     
-    
+#=============================
+# end application views
+#=============================
