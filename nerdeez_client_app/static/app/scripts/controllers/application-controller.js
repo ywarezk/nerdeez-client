a0330252a53f321f1dd0bfb7f58aca8e65fb0693
Doron <doron@doron-VirtualBox.(none)>
2013-07-21 20:18:49 +0300

    deleted application controller

--- nerdeez_client_app/static/app/scripts/controllers/application-controller.js
+++
@@ -1,7 +0,0 @@
-Nerdeez.ApplicationController = Ember.Controller.extend({
-  // Implement your controller here.
-});
-
-Nerdeez.AboutController = Ember.ArrayController.extend({
-	content: null,
-});
