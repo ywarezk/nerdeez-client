a79459e0655c2a587f9948b59f0fcdea8f7ca363
ywarezk <ywarezk@gmail.com>
2013-09-28 13:42:40 +0200

    trying to add the nerdeez-scripts submodule

---
+++ nerdeez_client_app/static/app/scripts/controllers/add-schoolgroup-controller.js
@@ -0,0 +1,12 @@
+/**
+ * The controller for the add school group will be defined here
+ * 
+ * Created September 28th, 2013
+ * @author: Yariv Katz
+ * @version: 1.0
+ * @copyright nerdeez Ltd.
+ */
+
+Nerdeez.AddSchoolGroupController = Ember.Controller.extend({
+	
+});
